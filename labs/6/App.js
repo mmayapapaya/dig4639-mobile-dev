fd9083c2310d47fdde7648060f84b74d90ea9f76
mmayapapaya <maya@lindseth.org>
2020-04-05 16:54:53 -0400

    lab6 updates, i need a mimosa

--- labs/6/App.js
+++ labs/6/App.js
@@ -1,6 +1,5 @@
 import React from 'react'
-import {StyleSheet} from 'react-native'
-import {Text, View} from 'react-native'
+import {StyleSheet, Text, View} from 'react-native'
 import {Card, Button} from 'react-native-elements'
 import questions from './questions.json'
 
@@ -19,53 +18,39 @@ export default class App extends React.Component {
     }
   }
 
-  nextQuestion(t) {
+  nextQuestion(q) {
     //clearInterval(this.timer)
     //this.setState({
       //titleText:"You're Speedy!",
       //currentState: TITLE_STATE
     //})
-    if (t.correct){
-      this.setState({counter: this.state.currentQuestion + 1})
+    if (q.correct){
+      this.setState({counter: this.state.counter + 1})
     }
       this.setState({currentQuestion: this.state.currentQuestion + 1})
+    }
 
-  }
-
-  start() {
-    this.setState({counter:0})
-    this.setState({currentState: QUESTION_STATE})
-    this.timer = setInterval(() => {
-    this.setState({counter : this.state.counter+1})
-    if(this.state.counter < this.timeLimit) {
-    this.setState({titleText:"Start Quiz" + this.state.counter})
-} else {
-  this.setState({titleText:"Time's Up."})
-  clearInterval(this.timer)
-}
-}, 3000);
-}
 
   render () {
     return (
-      <Card style={styles.container}>
+      <View style={styles.container}>
         {(this.state.currentState === TITLE_STATE) ? 
 
           <>
             <Text style={styles.text}>Personality Quiz for Lab Six</Text>
             <Button 
-              title="Begin Quiz" 
-              buttonStyle={styles.button} 
+              style={styles.button}
+              title="Start Quiz!" 
               onPress={ () => this.setState({ currentState: QUESTION_STATE })}
             />
           </>
 
           : (this.state.currentQuestion < questions.length) ? 
             <>
-              <Text style={styles.question}>{questions[this.state.currentQuestion].question}</Text>
+              <Text style={styles.text}>{questions[this.state.currentQuestion].question}</Text>
               <View>
                 {questions[this.state.currentQuestion].answers.map((ans, i) => {
-                  return <Button title={ans.text} key={i} buttonStyle={styles.button} onPress={() => this.nextQuestion(ans)} />
+                  return <Button style={styles.button} title={ans.text} key={i} onPress={() => this.nextQuestion(ans)} />
                 })}
               </View>
             </>
@@ -73,25 +58,35 @@ export default class App extends React.Component {
           : 
             
             <>
-              <Text style={styles.question}>Finished!</Text>
-              <Text style={styles.score}>Your Score: {this.state.score}/{questions.length}</Text>
-              <Button title="Start Over" buttonStyle={styles.button} onPress={() => this.setState({ currentState: TITLE_STATE, currentQuestion: 0, score: 0 })}/>
+              <Text style={styles.text}>Finished!</Text>
+              <Text style={styles.text}>Your Score: {this.state.counter}/{questions.length}</Text>
+              <Button style={styles.button} title="Start Over" onPress={() => this.setState({ currentState: TITLE_STATE, currentQuestion: 0, counter: 0 })}/>
             </>
         }
-      </Card>
+      </View>
     )
   }
 }
  
 const styles = StyleSheet.create({
+
   container: {
     flex: 1,
-    backgroundColor: 'plum',
+    backgroundColor: '#AFEEEE',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  button: {
+    padding: 10,
+  },
+
     text: {
+    padding: 10,
+    textAlign: 'center',
     fontSize: 18,
-    color: 'navy',
+    fontWeight: "bold",
+    color: '#4682B4'
   }
-});
+
+})
