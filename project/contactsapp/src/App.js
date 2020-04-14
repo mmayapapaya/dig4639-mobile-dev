b33cfe94ee6dc3ba7d300fb0b0d6972e05bf75ae
mmayapapaya <maya@lindseth.org>
2020-04-14 14:59:30 -0400

    start final

---
+++ project/contactsapp/src/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {contacts: []};
+
+  }
+
+  componentDidMount() {
+
+    window.fetch("http://plato.mrl.ai:8080/contacts", {headers: {API: "lindseth"}})
+    .then((res) => res.json())
+    .then((data) => {
+      this.setState({contacts: data.contacts});
+    });
+
+  }
+
+  render() {
+    return (
+      <div>
+       {
+         this.state.contacts.map((value, index) => {
+           return <p key={index}>{value.name}</p>;
+         })
+       }
+      </div>
+    );
+  }
+}
+
+export default App;
